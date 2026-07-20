82fa67dfe87b5d554b355187f0c02593a5f08000
Craig McLure <craig@mclure.net>
2026-07-20 22:41:21 +0100

    Run up an audio listener when Config tab is opened

--- src/ui/audio_pages/config.rs
+++ src/ui/audio_pages/config.rs
@@ -1,3 +1,4 @@
+use crate::device_manager::DeviceDefinition;
 use crate::ui::audio_pages::AudioPage;
 use crate::ui::audio_pages::config_pages::ConfigPage;
 use crate::ui::audio_pages::config_pages::compressor::CompressorPage;
@@ -6,14 +7,20 @@ use crate::ui::audio_pages::config_pages::headphones::HeadphonesPage;
 use crate::ui::audio_pages::config_pages::mic_equaliser::MicEqualiser;
 use crate::ui::audio_pages::config_pages::mic_setup::MicSetupPage;
 use crate::ui::audio_pages::config_pages::suppressor::NoiseSuppressionPage;
+use crate::ui::audio_pages::pipewire::device_locater::{
+    PipeWireNodeType, find_pipewire_nodes_for_usb,
+};
+use crate::ui::audio_pages::pipewire::spectrum::{SpectrumHandle, start_spectrum_analyser};
 use crate::ui::states::audio_state::BeacnAudioState;
 use crate::ui::widgets::draw_range;
+use crate::window_handle::{UserEvent, send_user_event};
 use beacn_lib::audio::messages::headphones::HPMicOutputGain;
 use beacn_lib::types::HasRange;
-use egui::{Ui, vec2};
+use egui::{Context, Ui, vec2};
 
 pub struct Configuration {
     equaliser: Box<MicEqualiser>,
+    spectrum_handler: Option<SpectrumHandle>,
 
     selected_tab: usize,
     tab_pages: Vec<Box<dyn ConfigPage>>,
@@ -23,6 +30,7 @@ impl Configuration {
     pub fn new() -> Self {
         Self {
             equaliser: Box::new(MicEqualiser::new()),
+            spectrum_handler: None,
 
             selected_tab: 0,
             tab_pages: vec![
@@ -99,7 +107,54 @@ impl AudioPage for Configuration {
         });
     }
 
+    fn on_page_open(&mut self, ctx: &Context, device: &DeviceDefinition) {
+        if self.spectrum_handler.is_some() {
+            // We already have a handler, do nothing.
+            return;
+        }
+
+        // Attempt to locate this devices audio channels
+        let location = device.location;
+        let nodes = find_pipewire_nodes_for_usb(location.bus_number, location.address);
+
+        let mut use_node = None;
+        if let Ok(nodes) = nodes {
+            // We found something, we need to find the mic node
+            for node in nodes {
+                if node.node_type == PipeWireNodeType::Source {
+                    if node.channels == 4 {
+                        use_node.replace(node);
+                    }
+                }
+            }
+        }
+
+        if let Some(node) = use_node {
+            // Ok, we have a usable node, let's fire up a listener..
+            let handler = start_spectrum_analyser(&*node.name, 48000);
+
+            // Get the internal Spectrum Data
+            let data = handler.data.clone();
+            self.equaliser.set_spectrum(data);
+
+            // Tell the UI to set the minimum refresh rate to 30Hz
+            send_user_event(ctx, UserEvent::SetMinimumRefreshRate(true));
+
+            self.spectrum_handler = Some(handler);
+        }
+    }
+
+    fn on_page_close(&mut self, ctx: &Context, _: &DeviceDefinition) {
+        if let Some(handler) = self.spectrum_handler.take() {
+            handler.stop();
+            send_user_event(ctx, UserEvent::SetMinimumRefreshRate(false));
+        }
+    }
+
     fn on_close(&mut self) {
+        if let Some(handler) = self.spectrum_handler.take() {
+            handler.stop();
+        }
         self.equaliser.clear();
     }
 }
