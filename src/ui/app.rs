69f01c2fc74607c579c83057d9a6b8785362f880
Craig McLure <craig@mclure.net>
2026-07-22 10:43:27 +0100

    Fix backtracking from global page to device

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -276,7 +276,7 @@ impl BeacnMicApp {
                     if page.show_on_error() == error
                         && (page.should_show(device_state))
                         && round_nav_button(ui, page.icon(), selected).clicked()
-                        && (self.active_device != Some(device.clone()) || self.active_page != index)
+                        && !selected
                     {
                         action = Some((device.clone(), index));
                     }
@@ -315,7 +315,7 @@ impl BeacnMicApp {
                     );
                     if page.show_on_error() == error
                         && round_nav_button(ui, page.icon(), selected).clicked()
-                        && (self.active_device != Some(device.clone()) || self.active_page != index)
+                        && !selected
                     {
                         action = Some((device.clone(), index));
                     }
