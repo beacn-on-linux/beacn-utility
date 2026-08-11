c24fec6690c26b2fb7d662c6010de244da07234b
Craig McLure <craig@mclure.net>
2026-08-11 06:14:47 +0100

    Initial Page trait

--- src/ui_iced/app.rs
+++ src/ui_iced/app.rs
@@ -1,5 +1,35 @@
+use crate::devices::manager::DeviceDefinition;
+use crate::devices::states::State;
+use crate::devices::states::audio::AudioState;
+use crate::devices::states::control::ControlState;
+use beacn_lib::manager::DeviceLocation;
+
 #[derive(Debug, Clone)]
 pub enum Message {
     ActivatePipeweaver,
     ActivateSettings,
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// This should probably be separated, but it's only a small abstraction
+pub enum DeviceState {
+    Audio(AudioState),
+    Control(ControlState),
+}
+
+impl State for DeviceState {
+    fn location(&self) -> &DeviceLocation {
+        match self {
+            DeviceState::Audio(state) => state.location(),
+            DeviceState::Control(state) => state.location(),
+        }
+    }
+
+    fn definition(&self) -> &DeviceDefinition {
+        match self {
+            DeviceState::Audio(state) => state.definition(),
+            DeviceState::Control(state) => state.definition(),
+        }
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////
