f77baeb27b9bf1b57e806ddfcf3b902ca4a62a64
Craig McLure <craig@mclure.net>
2026-08-18 22:31:58 +0100

    Handle all mouse controls

--- src/ui/pages/audio/hp_equaliser.rs
+++ src/ui/pages/audio/hp_equaliser.rs
@@ -1,14 +1,17 @@
-use crate::devices::states::audio::{AudioState, EqualiserBand};
-use crate::ui::pages::audio::hp_equaliser::HPEQMessage::{
-    Balance, Equaliser, Stereo, SubWoofer, ToggleLinked,
-};
+use crate::devices::states::audio::{AudioState, EqualiserBand, EqualiserBandType};
+use crate::ui::pages::audio::hp_equaliser::HPEQMessage::*;
+use crate::ui::pages::audio::hp_equaliser::HPEQValue::*;
+
 use crate::ui::pages::page::{AudioPage, PageMessage};
-use crate::ui::widgets::equaliser::eq_common::Bands;
+use crate::ui::widgets::equaliser::eq_common::{
+    Bands, EqGeometry, MAX_FREQUENCY, MAX_GAIN, MIN_FREQUENCY, MIN_GAIN, band_type_has_gain,
+    get_q_delta,
+};
 use crate::ui::widgets::equaliser::eq_drawer::{EQDrawView, EQMouseEvent};
 use crate::ui::widgets::helpers::slider::{slider_theme, themed_slider};
 use crate::ui::widgets::helpers::svg::{svg_button_style, svg_button_unstyled};
 use beacn_lib::audio::messages::Message;
-use beacn_lib::audio::messages::equaliser::EQBand;
+use beacn_lib::audio::messages::equaliser::{EQMode, EQQ};
 use beacn_lib::audio::messages::headphones::{HPLevel, HPMicMonitorLevel, Headphones};
 use beacn_lib::audio::messages::subwoofer::{Subwoofer, SubwooferAmount};
 use beacn_lib::manager::DeviceType;
@@ -19,13 +22,17 @@ use iced::font::Weight;
 use iced::mouse::ScrollDelta;
 use iced::widget::{Canvas, Space, checkbox, column, container, row, stack, text};
 use iced::{Alignment, Background, Border, Color, Element, Font, Length, Padding, Point, Task};
+use log::warn;
 use std::ops::RangeInclusive;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
 
+// Stolen from mic_equaliser
+const DRAG_DELAY: Duration = Duration::from_millis(80);
+
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Enum, EnumIter)]
-enum Channel {
+pub(crate) enum Channel {
     Left,
     Right,
 }
@@ -42,6 +49,12 @@ impl Channel {
 pub enum HPEQMessage {
     Equaliser(Channel, EQMouseEvent),
 
+    // EQ UI Elements
+    EqualiserValue(HPEQValue),
+
+    AddBand,
+    RemoveBand,
+
     Balance(i32),
     SubWoofer(u8),
     Stereo(bool),
@@ -49,6 +62,16 @@ pub enum HPEQMessage {
 
     // Direct State Change
     State(Message),
+}
+
+// We isolate these because they all basically do the same thing but with a different field,
+// so we can wrap up stuff for ease of consistency.
+#[derive(Debug, Clone)]
+pub enum HPEQValue {
+    Type(EqualiserBandType),
+    Frequency(u32),
+    Gain(f32),
+    Q(f32),
 }
 
 pub struct HPEqualiser {
@@ -62,7 +85,7 @@ pub struct HPEqualiser {
     active_channel: Channel,
     active_band: Option<EqualiserBand>,
 
-    drag_active: bool,
+    // Time in which a drag was initiated, if any.
     drag_start: Option<Instant>,
 
     // Needs to be fed from the state, currently unavailable.
@@ -79,8 +102,6 @@ impl HPEqualiser {
 
             active_channel: Channel::Left,
             active_band: None,
-
-            drag_active: false,
             drag_start: None,
 
             balance: 0,
@@ -94,6 +115,7 @@ impl HPEqualiser {
             self.temp[channel] = state.equaliser.bands[state.equaliser.mode];
             self.view[channel].set_bands(self.temp[channel].clone());
         }
+        self.switch_active_channel_force(Channel::Left, true);
     }
 
     fn update(&mut self, state: &mut AudioState, msg: HPEQMessage) -> Task<HPEQMessage> {
@@ -101,6 +123,66 @@ impl HPEqualiser {
             Equaliser(channel, event) => {
                 self.handle_eq_event(state, channel, event);
             }
+
+            EqualiserValue(value) => {
+                let channel = self.active_channel;
+                let other = self.active_channel.other();
+                let messages = vec![];
+                if let Some(band) = self.active_band {
+                    match value {
+                        Type(band_type) => {
+                            if let Some(band) = self.active_band {
+                                // TODO - MESSAGE
+                                self.temp[channel][band].band_type = band_type;
+
+                                if self.is_linked {
+                                    self.temp[other][band].band_type = band_type;
+                                }
+                            }
+                        }
+                        Frequency(frequency) => {
+                            if let Some(band) = self.active_band {
+                                // TODO - MESSAGE
+                                self.temp[channel][band].frequency = frequency;
+
+                                if self.is_linked {
+                                    self.temp[other][band].frequency = frequency;
+                                }
+                            }
+                        }
+                        Gain(gain) => {
+                            if let Some(band) = self.active_band {
+                                // TODO - MESSAGE
+                                self.temp[channel][band].gain = gain;
+
+                                if self.is_linked {
+                                    self.temp[other][band].gain = gain;
+                                }
+                            }
+                        }
+                        Q(q) => {
+                            if let Some(band) = self.active_band {
+                                // TODO - MESSAGE
+                                self.temp[channel][band].q = q;
+                                if self.is_linked {
+                                    self.temp[other][band].q = q;
+                                }
+                            }
+                        }
+                    }
+                    for message in messages {
+                        let _ = state.handle_message(message);
+                    }
+
+                    self.view[channel].set_band(band, self.temp[channel][band]);
+                    if self.is_linked {
+                        self.view[other].set_band(band, self.temp[other][band]);
+                    }
+                }
+            }
+
+            HPEQMessage::AddBand => {}
+            HPEQMessage::RemoveBand => {}
 
             Balance(amount) => {
                 self.balance = amount;
@@ -119,10 +201,10 @@ impl HPEqualiser {
             }
 
             ToggleLinked => {
-                self.is_linked = !self.is_linked;
+                self.set_linked(!self.is_linked);
             }
 
-            HPEQMessage::State(msg) => {
+            State(msg) => {
                 let _ = state.handle_message(msg);
             }
         }
@@ -135,15 +217,155 @@ impl HPEqualiser {
             EQMouseEvent::Pressed(e) => self.handle_eq_press(state, channel, e),
             EQMouseEvent::Moved(e) => self.handle_eq_moved(state, channel, e),
             EQMouseEvent::Released => self.handle_eq_released(state, channel),
-            EQMouseEvent::Scrolled { position, delta } => self.handle_eq_scrolled(state, channel, position, delta),
+            EQMouseEvent::Scrolled(p, d) => self.handle_eq_scrolled(state, channel, p, d),
         }
     }
 
-    fn handle_eq_press(&mut self, state: &mut AudioState, channel: Channel, point: Point) {}
-    fn handle_eq_moved(&mut self, state: &mut AudioState, channel: Channel, point: Point) {}
-    fn handle_eq_released(&mut self, state: &AudioState, channel: Channel) {}
+    fn handle_eq_press(&mut self, state: &mut AudioState, channel: Channel, point: Point) {
+        // Do we have a hit on a band?
+        if self.check_band_hit(channel, point).is_some() {
+            // Prepare for a drag, will be taken care of during the handle_eq_move callback
+            self.view[channel].set_track_motion(true);
+            self.drag_start = Some(Instant::now());
+        }
+    }
+    fn handle_eq_moved(&mut self, state: &mut AudioState, channel: Channel, point: Point) {
+        if self.drag_start.is_some_and(|t| t.elapsed() >= DRAG_DELAY) {
+            // We're dragging the active band, so should be updating its state
+            let Some(band) = self.active_band else {
+                return;
+            };
+
+            let plot = self.view[channel].plot_rect();
+
+            // Grab the Frequency..
+            let frequency = EqGeometry::x_to_freq(point.x, plot);
+            let frequency = frequency.clamp(MIN_FREQUENCY as f32, MAX_FREQUENCY as f32);
+
+            // TODO - MESSAGE
+            // Logically, this makes sense to queue messages, process them all at once, then apply the changes.
+            self.temp[channel][band].frequency = frequency as u32;
+            if self.is_linked {
+                self.temp[channel.other()][band].frequency = frequency as u32;
+            }
+
+            // Set the Gain..
+            if band_type_has_gain(self.temp[channel][band].band_type) {
+                let gain = EqGeometry::y_to_db(point.y, plot).clamp(MIN_GAIN, MAX_GAIN);
+                let gain = (gain * 10.0).round() / 10.0;
+
+                // TODO - MESSAGE
+                self.temp[channel][band].gain = gain;
+                if self.is_linked {
+                    self.temp[channel.other()][band].gain = gain;
+                }
+            }
+
+            self.view[channel].set_band(band, self.temp[channel][band]);
+            if self.is_linked {
+                self.view[channel.other()].set_band(band, self.temp[channel.other()][band]);
+            }
+        }
+    }
+    fn handle_eq_released(&mut self, state: &AudioState, channel: Channel) {
+        self.view[channel].set_track_motion(false);
+        self.drag_start = None;
+    }
     fn handle_eq_scrolled(&mut self, state: &AudioState, c: Channel, p: Point, d: ScrollDelta) {
-        let (channel, point, delta) = (&c, &p, &d);
+        let (channel, point, delta) = (c, p, d);
+
+        if let Some(band) = self.check_band_hit(channel, point) {
+            let delta = get_q_delta(delta);
+
+            let q = self.temp[channel][band].q;
+            let adjusted = (q + delta).clamp(0.1, 10.0);
+            let adjusted = (adjusted * 10.0).round() / 10.0;
+
+            // TODO - MESSAGE
+            self.temp[channel][band].q = adjusted;
+            self.view[channel].set_band(band, self.temp[channel][band]);
+
+            if self.is_linked {
+                self.temp[channel.other()][band].q = adjusted;
+                self.view[channel.other()].set_band(band, self.temp[channel.other()][band]);
+            }
+        }
+    }
+
+    // Checks for a band hit, if found, updates the channel and active band, then returns the band.
+    fn check_band_hit(&mut self, channel: Channel, point: Point) -> Option<EqualiserBand> {
+        let bands = &self.temp[channel];
+        let rect = self.view[channel].plot_rect();
+        if let Some(selected_band) = EqGeometry::hit_test(rect, point, bands) {
+            self.switch_active_channel(channel);
+
+            self.active_band = Some(selected_band);
+            self.view[channel].set_active(Some(selected_band));
+            if self.is_linked {
+                // If we're linked, update the other channel as well.
+                self.view[channel.other()].set_active(Some(selected_band));
+            }
+
+            return Some(selected_band);
+        }
+        None
+    }
+
+    // When we become linked, outside of syncing both channels, we also need a couple of display
+    // tweaks. Firstly, we disable the green border around the active channel, then we enable
+    // a matching active band on the other channel.
+    fn set_linked(&mut self, is_linked: bool) {
+        self.is_linked = is_linked;
+        if is_linked {
+            self.view[self.active_channel].set_border_colour(None);
+            self.view[self.active_channel.other()].set_active(self.active_band);
+        } else {
+            self.view[self.active_channel].set_border_colour(Some(Color::from_rgb8(0, 255, 0)));
+            self.view[self.active_channel.other()].set_active(None);
+        }
+    }
+
+    fn switch_active_channel(&mut self, channel: Channel) {
+        self.switch_active_channel_force(channel, false);
+    }
+
+    // The force param will result in the border being cleared then reloaded, along with the
+    // active band.
+    fn switch_active_channel_force(&mut self, channel: Channel, force: bool) {
+        if self.active_channel == channel && !force {
+            return;
+        }
+
+        // Clear the Active Band and the Border Colour from the existing channel
+        self.view[self.active_channel].set_active(None);
+        self.view[self.active_channel].set_border_colour(None);
+
+        // The active band is likely about to be replaced by the caller (normally a mouse down, or
+        // scroll event are the things that cause a channel switch), but that'll happen after a hit
+        // on a visible band point.
+        //
+        // With that said, safety first. Make sure a band is selected by default.
+
+        // Check whether the current active band is available on the new channel
+        let find_new_band = match self.active_band {
+            Some(band) => self.temp[channel][band].enabled,
+            None => true,
+        };
+
+        // If it is, set the new active band, otherwise use the current active band
+        let active_band = match find_new_band {
+            true => EqualiserBand::iter().find(|&b| self.temp[channel][b].enabled),
+            false => self.active_band,
+        };
+
+        self.active_channel = channel;
+        self.active_band = active_band;
+        self.view[channel].set_active(active_band);
+
+        // Only update the border if we're not linked
+        if !self.is_linked {
+            self.view[channel].set_border_colour(Some(Color::from_rgb8(0, 255, 0)));
+        }
     }
 
     fn view(&self, state: &AudioState) -> Element<'_, HPEQMessage> {
@@ -180,9 +402,9 @@ impl HPEqualiser {
         };
 
         // Should depend on whether self.active = active
-        let label_colour = match active {
-            Channel::Left => Color::from_rgba8(255, 255, 255, 0.5),
-            Channel::Right => Color::from_rgba8(0, 255, 0, 1.0),
+        let label_colour = match self.active_channel == active && !self.is_linked {
+            true => Color::from_rgba8(0, 255, 0, 1.0),
+            false => Color::from_rgba8(255, 255, 255, 0.5),
         };
 
         let label_font = Font {
